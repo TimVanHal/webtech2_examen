b574e46a47dc9549f7cf7cb553c1cc1d239aae19
TimVanHal <tim.vanhal@student.ap.be>
2016-01-20 16:37:48 +0100

    Added couchapp project

---
+++ Webtech2_examen_couchapp/app/_attachments/scripts/main.js
@@ -0,0 +1,55 @@
+function createDoc(){
+	
+	var entrydate = $("#entrydate").val();
+	var enddate = $("#enddate").val();
+	var status = $("#status").val();
+	var desc = $("#desc").val();
+	var prio = $("#prio").val();
+	
+	var doc = {};
+	
+	doc.entrydate = entrydate;
+	doc.enddate = enddate;
+	doc.status = status;
+	doc.desc = desc;
+	doc.prio = prio;
+	var json = JSON.stringify(doc);
+		
+	$.ajax({
+		type:	'POST',
+		url:	'../../',
+		data:	json,
+		contentType:	'application/json',
+		async:	true,
+		success:function(data){
+			buildOutput();
+		},
+		error:	function(XMLHttpRequest, textStatus, errorThrown){
+			console.log(errorThrown);
+		}
+	});
+}
+
+function buildOutput(){
+	var html = "<table>";
+	$.ajax({
+		type:		"GET",
+		url:		"../../_all_docs?include_docs=true",
+		async:		true,
+		success:	function(data){
+			var arr = JSON.parse(data).rows;
+			for(var i=0; i<arr.length; i++){
+				if(arr[i].id.indexOf('_design') == -1){
+					var doc = arr[i].doc;
+					html += "<tr><td>" + doc.entrydate + "</td><td>" + doc.enddate + "</td><td>" + doc.prio + "</td><td>" + doc.desc + "</td><td>" + doc.status + "</td></tr>";
+				}
+			}
+			
+			html += "</table>";
+			$('#output').html(html);
+		},
+		error:		function(XMLHttpRequest, textStatus, errorThrown){
+			console.log(errorThrown);
+		}
+	});
+}
